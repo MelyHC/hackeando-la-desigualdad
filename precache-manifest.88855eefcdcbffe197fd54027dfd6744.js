2893bebc6acf92002537edd6d41f78b2fb51c481
Mely <mhidalgo@inticousa.com>
2019-03-30 18:28:40 -0500

    Updates

---
+++ precache-manifest.88855eefcdcbffe197fd54027dfd6744.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "64a339b7836030fdbe40",
+    "url": "/hackeando-la-desigualdad/static/css/main.83021543.chunk.css"
+  },
+  {
+    "revision": "64a339b7836030fdbe40",
+    "url": "/hackeando-la-desigualdad/static/js/main.b5b222eb.chunk.js"
+  },
+  {
+    "revision": "9d83b0ce9730b208dd63",
+    "url": "/hackeando-la-desigualdad/static/js/runtime~main.5fe5cb09.js"
+  },
+  {
+    "revision": "5b43a1888ea5c900fc6d",
+    "url": "/hackeando-la-desigualdad/static/js/2.8f1980f3.chunk.js"
+  },
+  {
+    "revision": "e46da2bd81bb6cf819ac0d0faf7ec91b",
+    "url": "/hackeando-la-desigualdad/index.html"
+  }
+];
